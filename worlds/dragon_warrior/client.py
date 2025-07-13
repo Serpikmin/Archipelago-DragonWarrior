8dd45c7b20375c023bab3f79e55fa9546c33a0a3
Serpikmin <peter_ghob@yahoo.ca>
2025-07-13 11:30:20 -0400

    Fixed minimal accessibility

--- worlds/dragon_warrior/client.py
+++ worlds/dragon_warrior/client.py
@@ -10,7 +10,7 @@ nes_logger = logging.getLogger("NES")
 logger = logging.getLogger("Client")
 
 EXPECTED_ROM_NAME = "DWAPV"
-EXPECTED_VERSION = "030"
+EXPECTED_VERSION = "031"
 
 class DragonWarriorClient(BizHawkClient):
     game = "Dragon Warrior"
