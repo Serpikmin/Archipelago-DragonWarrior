08d65ec73c8b277dcc1a93350c3d810ab1c8a4a9
Peter Ghobrial <peter_ghob@yahoo.ca>
2025-10-27 19:10:44 -0400

    Fix equipment_helper() calls

--- worlds/dragon_warrior/regions.py
+++ worlds/dragon_warrior/regions.py
@@ -171,7 +171,7 @@ def connect_regions(world: World) -> None:
     connect(world, world.player, region_names, 'Menu', names.tantegel_throne_room)
     connect(world, world.player, region_names, 'Menu', names.overworld)
     connect(world, world.player, region_names, names.overworld, names.strong_overworld,
-        equipment_helper(3, 3, 2, True))
+        equipment_helper(world, 3, 3, 2, True))
     connect(world, world.player, region_names, names.overworld, names.breconnary)
     connect(world, world.player, region_names, names.overworld, names.garinham, 
         equipment_helper(world, 2, 2, 1))
@@ -188,7 +188,7 @@ def connect_regions(world: World) -> None:
     connect(world, world.player, region_names, names.garinham, names.mountain_cave,
         equipment_helper(world, 3, 3, 2, random_map))
     connect(world, world.player, region_names, names.overworld, names.swamp_cave,
-        equipment_helper(4, 4, 2, random_map))
+        equipment_helper(world, 4, 4, 2, random_map))
 
 
     connect(world, world.player, region_names, names.rimuldar, names.rainbow_drop_shrine,
